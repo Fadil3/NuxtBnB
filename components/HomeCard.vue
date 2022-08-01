cf86eaa83261ab515c1e9356b58c7f7279c9785c
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-08-01 09:36:15 +0700

    create home card component

---
+++ components/HomeCard.vue
@@ -0,0 +1,29 @@
+<template lang="">
+  <div>
+    <img :src="home.images[0]" style="width: 200px" />
+    <br />
+    {{ home.title }}
+    <br />
+    {{ home.location.address }}
+    <br />
+    {{ home.location.city }}
+    {{ home.location.state }}
+    <br />
+    {{ home.guests }} guests
+    <br />
+    {{ home.pricePerNight }} / Night <br />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    home: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang=""></style>
