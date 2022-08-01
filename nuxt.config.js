b55be02703b9b9bbd08b89046dc47e1675014fec
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-08-01 09:45:38 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,17 @@
 export default {
-  components: true,
+  components: true, // auto-generate components from .vue files
+  head: {
+    titleTemplate: 'Mastering Nuxt.js - %s', // page title with template
+    htmlAttrs: {
+      lang: 'en', // menambahkan atribut lang
+    },
+    bodyAttrs: {
+      class: ['mys-style'], // menambahkan atribut class
+    },
+    meta: [
+      {
+        charset: 'utf-8', // menambahkan meta charset
+      },
+    ],
+  },
 }
