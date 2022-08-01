aee8d5774556fed2fb42d2763c0c1c0fba378abe
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-08-01 09:36:31 +0700

    use home card and loop based on data

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,26 @@
 <template lang="">
-  <div>Hello world!!!</div>
+  <div class="">
+    <div
+      v-for="home in homes"
+      style="float: left; margin: 10px"
+      :key="home.objectID"
+    >
+      <HomeCard :home="home" />
+    </div>
+  </div>
 </template>
+
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      homes: homes.slice(0, 3),
+    }
+  },
+  components: {
+    HomeCard,
+  },
+}
 </script>
+
 <style lang=""></style>
